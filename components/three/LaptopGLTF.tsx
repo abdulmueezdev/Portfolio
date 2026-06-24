d8697f3a39c6bced58e874deaa1b49a301851eff
Abdul Mueez <abdulmueezshahid550@gmail.com>
2026-06-25 03:38:03 +0500

    fix: use fallback primitive laptop model to prevent useGLTF crash on dead model URL

--- components/three/LaptopGLTF.tsx
+++ components/three/LaptopGLTF.tsx
@@ -1,35 +1,10 @@
 'use client'
 import React from 'react'
-import { useGLTF, Html } from '@react-three/drei'
+import { Html } from '@react-three/drei'
 import { useSpring, animated } from '@react-spring/three'
 import { TerminalText } from './TerminalText'
 
 export function LaptopGLTF({ isOpen, onClick }: { isOpen: boolean; onClick: () => void }) {
-  const gltf = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf') as any
-  
-  const { lidRot } = useSpring({
-    lidRot: isOpen ? -1.9 : 0, // open angle
-    config: { tension: 80, friction: 20 },
-  })
-
-  if (gltf?.nodes?.macbook && gltf?.nodes?.screen) {
-    // We successfully loaded the pmndrs macbook model structure
-    return (
-      <group onClick={onClick} dispose={null} position={[0, -0.5, 0]}>
-        <mesh geometry={gltf.nodes.macbook.geometry} material={gltf.materials.macbook} />
-        <animated.group position={[0, 0, -1.05]} rotation-x={lidRot}>
-          <mesh geometry={gltf.nodes.screen.geometry} material={gltf.materials.macbook} />
-          {isOpen && (
-            <Html position={[0, 1.3, -0.1]} transform distanceFactor={1.5} rotation-x={-0.1}>
-              <TerminalText />
-            </Html>
-          )}
-        </animated.group>
-      </group>
-    )
-  }
-
-  // Fallback if the nodes don't match exactly
   return <FallbackLaptopModel isOpen={isOpen} onClick={onClick} />
 }
 
@@ -64,4 +39,4 @@ function FallbackLaptopModel({ isOpen, onClick }: { isOpen: boolean; onClick: ()
   )
 }
 
-useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+
